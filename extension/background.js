2614151c068e68b9ebcfda8ad0fd109a468c92ff
Roy <loyii91400@gmail.com>
2025-04-11 20:14:19 +0800

    remove extension folder

--- extension/background.js
+++
@@ -1,9 +0,0 @@
-import './serviceWorker/message.js';
-import './serviceWorker/executeScript.js';
-
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
-
-
-// Alternatively, you can directly inject a script dynamically
